5a26f2a84bd088d338d04b6e664ba11abfc34846
Croederul <mihneacapabil@gmail.com>
2026-07-25 13:24:30 +0300

    Add CategoryMenu and Iconify icon support
    - Introduced CategoryMenu organism
    - Add @iconify/react dependency
    - Corrected some typos and Applied minor styling tweaks
    - Updated package-lock.json for Iconify.

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,6 @@ import { ShopPage } from './components/templates/ShopPage/ShopPage';
 import { I18nProvider } from './i18n';
 
 export const App: React.FC = () => {
-  const handleCategoryClick = () => {
-    console.log('Category clicked!');
-  };
-
   return (
     <I18nProvider>
       <ShopPage />
