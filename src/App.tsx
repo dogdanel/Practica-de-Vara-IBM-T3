1590b072f09a937330d649cd5c39612ffb067af8
Croederul <mihneacapabil@gmail.com>
2026-07-25 13:24:30 +0300

    Add CategoryMenu and Iconify icon support
    - Introduced CategoryMenu organism
    - Add @iconify/react dependency
    - Corrected some typos and Applied minor styling tweaks
    - Updated package-lock.json for Iconify.

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,10 @@
 import React from 'react';
-import { CategoryCard } from './molecules/CategoryCard/CategoryCard';
-import BakingMaterial from '../public/CategoryIcons/BakingMaterial.png';
+import { CategoryMenu } from './organisms/CategoryMenu/CategoryMenu';
 
 export const App: React.FC = () => {
-  const handleCategoryClick = () => {
-    console.log('Category clicked!');
-  };
-
   return (
-    <div style={{ padding: '20px', maxWidth: '300px' }}>
-      <CategoryCard
-        icon={BakingMaterial}
-        title="Baking Material"
-        count={1}
-        onClick={handleCategoryClick}
-      />
+    <div style={{ padding: '40px' }}>
+      <CategoryMenu />
     </div>
   );
 };
